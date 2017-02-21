8d476b40301f931277114369e42ee2b0df2cb2b4
Joshua <jyoung@student.neumont.edu>
2017-02-21 15:48:48 -0700

    Checking
    This updated version comes with a checker for all the text boxes, and comes with the onClick function for the submit button that checks everything that is put into the fields

--- fitness-gurus-Josh-UI/FitnessTracker/MainWindow.xaml.cs
+++ fitness-gurus-Josh-UI/FitnessTracker/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,7 +28,55 @@ namespace FitnessTracker
 
         private void savebtn_Click(object sender, RoutedEventArgs e)
         {
-
+            Boolean ageAccepted = false;
+            string ageRegex = "([0-9]+)";
+            Regex ageR = new Regex(ageRegex);
+            string age = agetbx.Text;
+            Boolean nameAccepted = false;
+            string nameRegex = "([A-Za-z ]+)";
+            Regex nameR = new Regex(nameRegex);
+            Boolean genderAccepted = false;
+            Boolean weightAccepted = false;
+            Boolean HeightAccepted = false;
+            string name = nametbx.Text;
+            Match m = nameR.Match(name);
+            Match a = ageR.Match(age);
+            if (weighttbx != null)
+            {
+                weightAccepted = true;
+            }
+            if (heighttbx != null)
+            {
+                HeightAccepted = true;
+            }
+            if (m.Success)
+            {
+                nameAccepted = true;
+            }
+            if (a.Success)
+            {
+                ageAccepted = true;
+            }
+            if ((gendertbx.Text == "Male" || gendertbx.Text == "Female"))
+            {
+                genderAccepted = true;
+            }
+            if (genderAccepted == true)
+            {
+                if (HeightAccepted == true)
+                {
+                    if (weightAccepted == true)
+                    {
+                        if (ageAccepted == true)
+                        {
+                            if (nameAccepted == true)
+                            {
+                                Profile.Close();
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
