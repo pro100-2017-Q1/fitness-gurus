e15b8ad7ffa0453fe3da62c001022896a601a14d
abruton <abruton@student.neumont.edu>
2017-02-27 17:53:53 -0700

    login fixed

--- FitnessTracker/Login.xaml.cs
+++ FitnessTracker/Login.xaml.cs
@@ -23,5 +23,33 @@ namespace FitnessTracker
         {
             InitializeComponent();
         }
+        private void forgottenPasswordLabel_Click(object sender, RoutedEventArgs e)
+        {
+            // where does it go from here ??
+        }
+        private void loginButtonChanged_Click(object sender, RoutedEventArgs e)
+        {
+            // if username and password are correct goes to other pages
+            // if username and password are incorrect, makes you try again
+        }
+        private void tbxUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // username: maximum 8 
+        }
+        private void tbxUsername_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tbx = (TextBox)sender;
+            tbx.Text = string.Empty;
+            tbx.GotFocus -= tbxUsername_GotFocus;
+        }
+        private void tbxPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            // password: maximum 8 > validate correct
+            // contains one lowercase
+            // contains one uppercase
+            // contains one number 
+            // Regex: 
+
+        }
     }
 }
