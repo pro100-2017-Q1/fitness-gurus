5cd28fa057ba417fe9fa157478024ba0b0817da9
Brad Gustafson <greeninja13@gmail.com>
2017-03-03 17:47:24 -0700

    minor change
    removed manual activities in the main window

--- FitnessTracker/MainWindow.xaml.cs
+++ FitnessTracker/MainWindow.xaml.cs
@@ -24,11 +24,7 @@ namespace FitnessTracker
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<CSVActivity> activityLog = new ObservableCollection<CSVActivity>
-         {
-             new CSVActivity("Walking", 150, 2),
-             new CSVActivity("Biking", 300, 3)
-         };
+        ObservableCollection<CSVActivity> activityLog = new ObservableCollection<CSVActivity>();
 
         public MainWindow()
         {
